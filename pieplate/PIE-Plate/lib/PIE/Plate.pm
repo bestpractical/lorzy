db6629d5de03965b8f5fdde6368cb8e5cd30c069
Jesse Vincent <jesse@bestpractical.com>
2008-05-25 03:16:23 +0000

    * Added the jquery ui stuff we'll be using
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/pie/branches/named-params@12670 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- pieplate/PIE-Plate/lib/PIE/Plate.pm
+++ pieplate/PIE-Plate/lib/PIE/Plate.pm
@@ -3,6 +3,9 @@ package PIE::Plate;
 Jifty->web->add_javascript(
     qw( jquery.superflydom.js
         jquery.jeditable.js
+        jquery.dimensions.js
+        jquery.ui-1.5b4/ui.core.js
+        jquery.ui-1.5b4/ui.sortable.js
         pieui.js ));
 
 
