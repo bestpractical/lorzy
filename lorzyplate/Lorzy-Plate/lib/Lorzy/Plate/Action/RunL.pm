7bf9cf996208d0a04fd06252ef2232e2e77f368a
Shawn Moore <sartak@bestpractical.com>
2008-07-08 16:28:07 -0400

    Perltidy the RunL action.. yikes :)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/Lorzy/trunk@13885 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lorzyplate/Lorzy-Plate/lib/Lorzy/Plate/Action/RunL.pm
+++ lorzyplate/Lorzy-Plate/lib/Lorzy/Plate/Action/RunL.pm
@@ -5,50 +5,51 @@ use Lorzy::Builder;
 use Lorzy::FunctionArgument;
 use Lorzy::Evaluator;
 use JSON;
+
 use Jifty::Param::Schema;
 use Lorzy::Plate::Action schema {
     param struct => type is 'text';
 };
 
-
 sub take_action {
-    my $self = shift;
-    warn $self->argument_value('struct');
-    my $tree = JSON::from_json($self->argument_value('struct'));
-    my $builder = Lorzy::Builder->new();
-    my $eval = Lorzy::Evaluator->new();
+    my $self    = shift;
+    my $tree    = JSON::from_json($self->argument_value('struct'));
+    my $builder = Lorzy::Builder->new;
+    my $eval    = Lorzy::Evaluator->new;
 
+    my $MATCH_REGEX = Lorzy::Lambda::Native->new(
+        body => sub {
+            my $args = shift;
+            my $arg   = $args->{'tested-string'};
+            my $regex = $args->{'regexp'};
+            return $arg =~ m/$regex/;
+        },
+        signature => {
+           'tested-string' => Lorzy::FunctionArgument->new(
+               name => 'tested-string',
+               type => 'Str'
+           ),
+           'regexp' => Lorzy::FunctionArgument->new(
+               name => 'regex',
+               type => 'Str',
+           ),
+        }
+    );
 
-my $MATCH_REGEX = Lorzy::Lambda::Native->new(
-    body => sub {
-        my $args = shift;
-        my $arg = $args->{'tested-string'};
-        my $regex = $args->{'regexp'};
+    $eval->set_global_symbol( 'match-regexp' => $MATCH_REGEX );
 
-        return $arg =~ m/$regex/;
-    },
+    eval {
+        my $script = $builder->defun(ops => $tree, signature => {});
+        $eval->apply_script($script, {});
+    };
 
-    signature => {
-           'tested-string' =>  Lorzy::FunctionArgument->new( name =>              'tested-string' =>  type => 'Str' ),
-           'regexp'=>  Lorzy::FunctionArgument->new( name =>      'regex', type => 'Str' )
-    }
-
-);
-$eval->set_global_symbol( 'match-regexp' => $MATCH_REGEX );
-
-
-    eval { 
-            my $script  = $builder->defun( ops => $tree, signature => {});
-
-            $eval->apply_script($script, {} )};
-    if (my $msg = $@) { 
+    if (my $msg = $@) {
         $self->result->error($msg);
-    }else {
-    warn $eval->result->value, $eval->result->success ;
-
-    $self->result->message($eval->result->value);
-   $eval->result->success ||  $eval->result->error($eval->result->error);
-}
+    } else {
+        warn $eval->result->value, $eval->result->success;
+        $self->result->message($eval->result->value);
+        $eval->result->success || $eval->result->error($eval->result->error);
+    }
 }
 
 1;
